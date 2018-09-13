5e7efa0d67fb308a585cc20cdc37e4e8d464d978
Teo <todorare@gmail.com>
2018-09-13 11:28:58 +0100

    Add experiment #003

--- 001/sketch.js
+++ 001/sketch.js
@@ -1,12 +1,12 @@
 let x, y;
 let counter = 0;
 
-// const capturer = new CCapture({
-//     format: 'gif',
-//     workersPath: 'js/',
-//     framerate: 30,
-//     verbose: true
-// });
+const capturer = new CCapture({
+    format: 'gif',
+    framerate: 30,
+    workersPath: 'js/',
+    verbose: true
+});
 
 function randomRGB() {
     return [random(0, 255), random(0, 255), random(0, 255)];
@@ -15,26 +15,28 @@ function randomRGB() {
 function setup() {    
     const p5Canvas = createCanvas(windowWidth, windowHeight);    
     canvas = p5Canvas.canvas;
-    frameRate(30);
     fill(0);
-    // capturer.start();
+    capturer.start();
 }
 
 function draw() {
-    // capturer.capture(canvas);
+    console.log(frameCount);
     counter += 1;
     x = random(0, width);
     y = random(0, width);
     strokeWeight(random(1,4));
     stroke(randomRGB());
     line(0, x, y, x)
-    if(counter > 200) {
+    if(counter < 200) {
+        capturer.capture(canvas);
+    }
+    if(counter === 200) {
         clear();
         counter = 0;
         // Save the animation as a .gif file
-        // noLoop();
-        // capturer.stop();
-        // capturer.save();
+        noLoop();
+        capturer.stop();
+        capturer.save();
     }
 }
 
---
+++ 003/sketch.js
@@ -0,0 +1,33 @@
+let x;
+let scaledStroke;
+const canvasHeight = 1080;
+const canvasWidth = 1080;
+function setup() {
+  createCanvas(canvasWidth, canvasHeight);
+
+  x = height;
+  for (let i = 0; i < height; i++) {
+    scaledStroke = map(x, canvasHeight, 0, 255, 0); 
+    stroke(scaledStroke);
+    line(0, i, width, i);
+    x--;
+  }
+  noStroke();
+  fill(0);
+  arc(height - 180, 180, 125, 125, PI, 2 * PI );
+  fill(255);
+  arc(height - 180, 180, 125, 125, 2 * PI, PI);
+  for (let i = 0; i < height; i += 70) {
+    scaledFill = map(i, 500, 0, 255, 0); 
+    fill(scaledFill, 210);
+    rect(width/2 - 10, i - 20, 10, 50);
+    rect(width/2 + 10, i - 20, 10, 50);
+  }
+  fill(255, 20);
+  for (let i = 0; i < width; i += 90) {
+    for (let j = 0; j < 60; j += 20) {
+      rect(i + 10, height - 20 - random(20, 40) - j, 60, 20);
+    }
+  }
+  saveCanvas('autobahn', 'jpg');
+}
